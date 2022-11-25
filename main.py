36fa004839f34837bbeedf59c4abff5dada75e90
Kazuki Noto <kazuki.noto.0206@gmail.com>
2022-11-25 07:41:00 +0000

    ページホスティング部分の修正

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+# page hosting
+app.mount("/", StaticFiles(directory="frontend/my-app/out", html=True), name="html")
